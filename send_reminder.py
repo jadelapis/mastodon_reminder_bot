b748269cd4d99fd0035864b5c5c8581a6af987f8
satoshi-sh <satoss1108@gmail.com>
2024-05-26 11:19:54 -0500

    Created script to make a remider post.

---
+++ send_reminder.py
@@ -0,0 +1,28 @@
+from mastodon import Mastodon
+from dotenv import load_dotenv
+import os
+from datetime import datetime
+
+
+def post_remider():
+    # Access the environment variables
+    secret_key = os.getenv('SECRET_KEY')
+    instance_url = os.getenv('API_BASE_URL')
+    account = os.getenv('MASTODON_ACCOUNT')
+    message = os.getenv("MESSAGE")
+    current_time = datetime.now()
+    greeting = "Good morning!! " if current_time.hour< 12 else "Good afternoon!! "
+    mastodon = Mastodon(
+        access_token=secret_key,
+        api_base_url=instance_url,
+    )
+    mastodon.status_post(f"{account}\n\n{greeting}{message}")
+
+
+def main():
+    load_dotenv()
+    post_remider()
+
+
+if __name__ == '__main__':
+    main()
